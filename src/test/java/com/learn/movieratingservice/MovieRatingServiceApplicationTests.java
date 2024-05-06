54c95f4142692eb9536cea580bfe9ddbff9d18ba
KarthikBenki <karthikmc007@gmail.com>
2024-05-06 09:18:23 +0530

    initialise movie-rating-service

---
+++ src/test/java/com/learn/movieratingservice/MovieRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.movieratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
