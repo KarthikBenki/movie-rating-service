79a0df6067b81338e94583eaf603291f2c54ddbb
KarthikBenki <karthikmc007@gmail.com>
2024-05-07 10:41:24 +0530

    create user rating object

--- src/main/java/com/learn/movieratingservice/controller/MovieRatingController.java
+++ src/main/java/com/learn/movieratingservice/controller/MovieRatingController.java
@@ -1,9 +1,14 @@
 package com.learn.movieratingservice.controller;
 
 import com.learn.movieratingservice.model.Rating;
+import com.learn.movieratingservice.model.UserRating;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +17,13 @@ public class MovieRatingController {
         @RequestMapping("/{movieId}")
         public Rating getRating(@PathVariable("movieId") String movieId) {
             return new Rating("Raja Huli",10);
+        }
+
+        @GetMapping("users/{userId}")
+        public UserRating getRatingsOfAUser(@PathVariable("userId") String userId){
+            List<Rating> ratings = Arrays.asList(new Rating("1234", 5), new Rating("5678", 2));
+            UserRating userRating = new UserRating();
+            userRating.setRatings(ratings);
+            return userRating;
         }
 }
